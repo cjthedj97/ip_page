383e28287bf0519ef858ad6f65d62576f45e7a56
CJ Saathoff <git@saathoff.tech>
2020-06-21 19:04:40 +0000

    shortened minimal url

--- index.php
+++ index.php
@@ -34,6 +34,6 @@
 <footer>
 <p></p>
 <p></p>
-<h5>For a minimal ip page go to <a href="https://ip.cjthedj97.me/m.php">ip.cjthedj97.me/m.php</a></h5>
+<h5>For a minimal ip page go to <a href="https://ip.cjthedj97.me/m/">ip.cjthedj97.me/m</a></h5>
 </footer>
 </html>
