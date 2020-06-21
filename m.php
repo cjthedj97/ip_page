e08707c766cefe8e58b102e9fb0d1a84d3e61342
CJ Saathoff <git@saathoff.tech>
2020-06-21 16:52:30 +0000

    minimal ip page added

--- m.php
+++ m.php
@@ -1,7 +1,7 @@
-<?php echo "\r\n" ?>
+<?php echo "" ?>
 
 <?php echo  "Your IP: "; echo $_SERVER["HTTP_CF_CONNECTING_IP"]; echo "\r\n"; ?>
 
 <?php echo "Country: "; echo $_SERVER["HTTP_CF_IPCOUNTRY"]; echo "\r\n"; ?>
 
-<?php echo "\r\n" ?>
+<?php echo "" ?>
