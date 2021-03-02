9f86f77c170a172640c729df8ce06ecc1c846ede
CJ Saathoff <cj@cjthedj97.com>
2021-03-02 17:04:24 +0000

    Update 'test.php'

--- test.php
+++ test.php
@@ -4,8 +4,6 @@ if(strpos($_SERVER['HTTP_USER_AGENT'], 'curl') !== FALSE)
    echo $_SERVER["REMOTE_ADDR"] , "\n";
  elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'WindowsPowerShell') !== FALSE)
    echo $_SERVER["REMOTE_ADDR"] , "n";
- elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== FALSE)
-   echo 'Google Chrome';
  else
    echo 'Something else';
 ?>
