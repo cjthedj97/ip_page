0cbca37f6df28d55507fc8529cceadefd525ef52
CJ Saathoff <cj@cjthedj97.com>
2021-03-02 15:20:00 +0000

    Update 'test.php'

--- test.php
+++ test.php
@@ -1,16 +1,12 @@
 <?php
 
-function get_browser_name($user_agent)
-{
-    if (strpos($user_agent, 'curl')) return 'curl';
-    elseif (strpos($user_agent, 'Chrome')) return 'Chrome';
- Explorer';
-   
-    return 'Other';
-}
-
-// Usage:
-
-echo get_browser_name($_SERVER['HTTP_USER_AGENT']);
-
+if(strpos($_SERVER['HTTP_USER_AGENT'], 'curl') !== FALSE)
+   echo 'curl';
+    echo 'Internet explorer';
+ elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Firefox') !== FALSE)
+   echo 'Mozilla Firefox';
+ elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== FALSE)
+   echo 'Google Chrome';
+ else
+   echo 'Something else';
 ?>
