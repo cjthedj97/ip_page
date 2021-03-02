fcc15f1a2e8831ce15654dcbfd8b29e0c1f1f1c5
CJ Saathoff <cj@cjthedj97.com>
2021-03-02 15:16:27 +0000

    Update 'test.php'

--- test.php
+++ test.php
@@ -1,31 +1,16 @@
 <?php
 
-function get_user_browser()
+function get_browser_name($user_agent)
 {
-    $u_agent = $_SERVER['HTTP_USER_AGENT'];
-    $ub = '';
-    if(strlen(strstr($u_agent,"curl")) > 0 ){ 
-
-      $ub = 'cl';
-
-    }
-    else {
-      $ub = 'other';
-    }
-
-    return $ub;
-} 
-
-if (isset($_GET['print']) && $_GET['print'] != "" 
-                          && get_user_browser() == 'cl') 
-{
-    $pg = $_GET['print'];
-    if (!file_exists('1')) 
-    {
-        echo $_SERVER["REMOTE_ADDR"];
-    }
+    if (strpos($user_agent, 'curl')) echo 'curl';
+    elseif (strpos($user_agent, 'Chrome')) echo 'Chrome';
+ Explorer';
+   
+    echo 'Other';
 }
-else 
-{
-    echo ' code goes here';
-}
+
+// Usage:
+
+echo get_browser_name($_SERVER['HTTP_USER_AGENT']);
+
+?>
