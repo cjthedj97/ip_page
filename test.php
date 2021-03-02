90a54e49cc333ce74e29fb4633d0fb0db5050ebd
CJ Saathoff <cj@cjthedj97.com>
2021-03-02 17:44:59 +0000

    Update 'test.php'

--- test.php
+++ test.php
@@ -1,9 +1,2 @@
 <?php
-
-if(strpos($_SERVER['HTTP_USER_AGENT'], 'curl') !== FALSE)
-   echo $_SERVER["REMOTE_ADDR"] , "\n";
- elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'WindowsPowerShell') !== FALSE)
-   echo $_SERVER["REMOTE_ADDR"] , "n";
- else
-   include 'desktop_main.php';
-?>
+echo '';
