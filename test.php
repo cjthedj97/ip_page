1472f5c1ef24df0c95ba4ff344146053776f5455
CJ Saathoff <cj@cjthedj97.com>
2021-03-02 15:23:43 +0000

    Update 'test.php'

--- test.php
+++ test.php
@@ -2,8 +2,8 @@
 
 if(strpos($_SERVER['HTTP_USER_AGENT'], 'curl') !== FALSE)
    echo 'curl';
- elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Firefox') !== FALSE)
-   echo 'Mozilla Firefox';
+ elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'WindowsPowerShell') !== FALSE)
+   echo 'Powershell';
  elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== FALSE)
    echo 'Google Chrome';
  else
