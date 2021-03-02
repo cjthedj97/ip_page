a4410f9be191133c3fc1adecc44b365073109eea
CJ Saathoff <cj@cjthedj97.com>
2021-03-02 21:30:06 +0000

    Update 'index.php'

--- index.php
+++ index.php
@@ -5,7 +5,22 @@ if(strpos($_SERVER['HTTP_USER_AGENT'], 'curl') !== FALSE)
  elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'WindowsPowerShell') !== FALSE)
    echo $_SERVER["REMOTE_ADDR"] , "\n";
  elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Go-http-client') !== FALSE)
+  echo $_SERVER["REMOTE_ADDR"] , "\n"; 
+ elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Wget') !== FALSE)
    echo $_SERVER["REMOTE_ADDR"] , "\n"; 
+ elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Lunx') !== FALSE)
+   echo $_SERVER["REMOTE_ADDR"] , "\n"; 
+ elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'libwww-perl') !== FALSE)
+   echo $_SERVER["REMOTE_ADDR"] , "\n";
+ elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'libcurl-agent') !== FALSE)
+  echo $_SERVER["REMOTE_ADDR"] , "\n"; 
+ elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Java') !== FALSE)
+   echo $_SERVER["REMOTE_ADDR"] , "\n"; 
+ elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'PycURL') !== FALSE)
+   echo $_SERVER["REMOTE_ADDR"] , "\n"; 
+    elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'PECL::HTTP') !== FALSE)
+   echo $_SERVER["REMOTE_ADDR"] , "\n";
+
 else
    include 'desktop_main.php';
 ?>
