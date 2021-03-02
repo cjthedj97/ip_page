e98e0012ba5082a0902cba0bcfa8649b80320250
CJ Saathoff <cj@cjthedj97.com>
2021-03-02 21:17:51 +0000

    Update 'index.php'

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ if(strpos($_SERVER['HTTP_USER_AGENT'], 'curl') !== FALSE)
    echo $_SERVER["REMOTE_ADDR"] , "\n";
  elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'WindowsPowerShell') !== FALSE)
    echo $_SERVER["REMOTE_ADDR"] , "n";
- else
+ elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Go-http-client') !== FALSE)
+   echo $_SERVER["REMOTE_ADDR"] , "n"; 
+else
    include 'desktop_main.php';
 ?>
